a205c58ee23c2bd6fa5fe596edcd950da99ab31f
Fox Zhou <yuan.zhou@eumlab.com>
2016-10-20 10:54:29 +0800

    修改背景

--- assets/library/index.js
+++ assets/library/index.js
@@ -46,16 +46,16 @@ registerEvents = function(){
   });
   if (win.width() >= 950) {
     $("#btn-show-mentor").mouseenter(function() {
-        $("section#mentor").addClass("mentor-hover");
-        $("section#mentor").removeClass("mentor-normal");
+        $("section#mentor #team-0").addClass("mentor-hover");
+        $("section#mentor #team-0").removeClass("mentor-normal");
         $("#mentor-detail").addClass("mentor-hover");
         $("#mentor-detail").removeClass("mentor-normal");
         $("#btn-show-mentor").html("了解更多 <span>></span>");
         $("#btn-show-mentor").attr("style", "visibility: visible; animation-delay: 0s; animation-name: fadeInUp;");
       })
       .mouseleave(function() {
-        $("section#mentor").removeClass("mentor-hover");
-        $("section#mentor").addClass("mentor-normal");
+        $("section#mentor #team-0").removeClass("mentor-hover");
+        $("section#mentor #team-0").addClass("mentor-normal");
         $("#mentor-detail").removeClass("mentor-hover");
         $("#mentor-detail").addClass("mentor-normal");
         $("#btn-show-mentor").html("了解更多");
